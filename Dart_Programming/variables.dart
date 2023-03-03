be13fd9899c784e028afa6ad318619b6dbb9dbc4
nicky-ops <nickyrutto@gmail.com>
2023-03-03 12:09:36 +0000

    Variables in Dart

---
+++ Dart_Programming/variables.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  // String 
+  String name = 'Nickson';
+  print(name);
+
+  // integer
+  int answer = 34;
+  print(answer);
+
+  // Decimal numbers
+  double price = 34.50;
+  print(price);
+}
