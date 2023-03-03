4f887f0f0c3b7cc91676527bf72856de45c02983
nicky-ops <nickyrutto@gmail.com>
2023-03-03 19:25:41 +0000

    return string length

---
+++ dartProgramming/dartFunctionsAssignment/stringLength.dart
@@ -0,0 +1,11 @@
+/* function takes an argument of type String and returns the length of that string
+*/
+
+int stringLength( String str)
+{
+  return str.length;
+}
+void main()
+{
+  print(stringLength("Javascript"));
+}
