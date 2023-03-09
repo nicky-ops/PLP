66b7287d772c790e2c7e195a3ffe1504c21f228d
nicky-ops <nickyrutto@gmail.com>
2023-03-09 09:53:07 +0000

    check if number is greater than, equal to or less than 10

---
+++ dartProgramming/controlFlow/dartControlFlowAssignment.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter a number");
+  int num = int.parse(stdin.readLineSync()!);
+  if (num >10)
+  {
+    print("Your number is greater than 10");
+  } else if (num < 10){
+    print("Your number is less than 10");
+  } else if (num == 10){
+    print("Your number is equal to 10");
+  }
+}
