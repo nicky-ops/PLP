c8d9d1a3418171e506abe9421d212e540c66ab7a
nicky-ops <nickyrutto@gmail.com>
2023-03-12 10:03:15 +0000

    funcions in python

---
+++ python3/functions.py
@@ -0,0 +1,5 @@
+def details:
+    full_name = input("Enter your first and last name")
+    email = input("Enter your email address")
+
+    print(f`My name is {full_name} and my email address is ${email}`)
