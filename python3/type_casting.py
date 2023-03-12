86d3c282cf33f22c8b94759836f5ae9be913d722
nicky-ops <nickyrutto@gmail.com>
2023-03-12 10:03:40 +0000

    type casting in python

---
+++ python3/type_casting.py
@@ -0,0 +1,15 @@
+# casting str to int
+str = '99'
+num1 = int(str)
+num2 = 67
+# Throws an error sum1 = str+num2
+sum2 = num1 + num2
+
+
+print(str,type(str))
+print(num1, type(num1))
+
+# printing sum 1 throws an error
+# print(sum1)
+
+print(sum2)
