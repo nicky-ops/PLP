3677bc99cb836fc5d6c7c924f6b5730a0d21aaf8
nicky-ops <nickyrutto@gmail.com>
2023-03-12 14:01:03 +0000

    arithmetic operators in python

---
+++ python3/arithmetic_operators.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 6
+
+print("Addition",num1+num2)
+print("Subtraction", num2-num1)
+print("Multiplication ", num1*num2)
+print("Division ", num2/num1)
+print("Floor Division ", num2//num1)
+print("Modulo ", num2%num1)
+print("Power", num1**num2)
