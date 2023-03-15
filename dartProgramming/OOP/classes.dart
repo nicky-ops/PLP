100655bee4f062d0fd2ad7ff61145353b4ce5cac
nicky-ops <nickyrutto@gmail.com>
2023-03-15 09:12:29 +0000

    classes in dart

---
+++ dartProgramming/OOP/classes.dart
@@ -0,0 +1,12 @@
+class Cat {
+  String name = 'Museo';
+  String color = 'RGB';
+}
+void main(){
+  var nora = Cat ();
+  nora.name = 'Zepphyrr';
+  nora.color = 'Grey';
+  print(nora);
+  print(nora.name);
+  print(nora.color);
+}
