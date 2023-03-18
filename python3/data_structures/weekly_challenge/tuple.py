dd71b7e981817a101be6b511a4233b8129f02d1e
nicky-ops <nickyrutto@gmail.com>
2023-03-18 16:46:18 +0000

    print elements of a tuple each on a newline

---
+++ python3/data_structures/weekly_challenge/tuple.py
@@ -0,0 +1,7 @@
+'''
+print each element of a tuple on a newline 
+'''
+
+books = ('Rich Dad Poor Dad','Atomic Habits','Meditations','Deep Work','Psychology of Money')
+for book in books:
+  print(f"{book}")
